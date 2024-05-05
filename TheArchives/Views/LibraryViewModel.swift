6b9728740290e723b7982b9cf704c56499c77ac6
DarkKnight8034 <darkknight8034@gmail.com>
2024-05-05 18:28:45 -0400

    create book view and lists library in library view

--- TheArchives/Views/LibraryViewModel.swift
+++ TheArchives/Views/LibraryViewModel.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
+import CoreData
 
 class LibraryViewModel: ObservableObject {
+    
+    private let context = PersistenceController.instance.context
+    
+    @Published var books: [Book] = []
+    
+    func loadLibrary() {
+        
+        let request: NSFetchRequest<Book> = Book.fetchRequest()
+        
+        do {
+            books = try context.fetch(request)
+        } catch {
+            print(error)
+        }
+        
+    }
     
 }
