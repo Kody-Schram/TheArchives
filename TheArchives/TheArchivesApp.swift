44fdff95b5cc4f5ac91408f4fb2ed2c856181932
DarkKnight8034 <darkknight8034@gmail.com>
2024-05-05 16:28:44 -0400

    Create project

--- TheArchives/TheArchivesApp.swift
+++ TheArchives/TheArchivesApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @main
 struct TheArchivesApp: App {
-    let persistenceController = PersistenceController.shared
+    let persistenceController = PersistenceController.instance
 
     var body: some Scene {
         WindowGroup {
